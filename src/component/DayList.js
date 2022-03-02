4bcf594d35aaceb90dc06f381149d0f4a775a73f
YoonSeoRoh <yoonseo1997@naver.com>
2022-03-02 18:46:10 +0900

    server

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function DayList() {
   const [days, setDays] = useState([]);
-  console.log(dummy);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/days")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setDays(data);
+      });
+  }, []);
   return (
     <ul className="list_day">
-      {dummy.days.map((day) => (
+      {days.map((day) => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}>Day {day.day}</Link>
         </li>
