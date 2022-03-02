6e3bdfc133eb960ac751c26be4c485d9716b461a
YoonSeoRoh <yoonseo1997@naver.com>
2022-03-02 21:43:39 +0900

    end

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,24 +1,14 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
 import Word from "./Word";
+import useFetch from "../hooks/useFetch";
 
 export default function Day() {
-  //const day = useParams().day;
   const { day } = useParams();
-  const [words, setWords] = useState([]);
-
-  useEffect(() => {
-    fetch(`http://localhost:3001/words?day=${day}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setWords(data);
-      });
-  }, [day]);
+  const words = useFetch(`http://localhost:3001/words?day=${day}`);
   return (
     <>
       <h2>Day{day}</h2>
+      {words.length === 0 && <span>Loading...</span>}
       <table>
         <tbody>
           {words.map((word) => (
